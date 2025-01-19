d31858119c50deb5773a867f0bec4f56819ed4f0
Felix Lindblom <felix.lindblom@students.fhnw.ch>
2025-01-19 17:44:15 +0100

    0.6.0 new page added

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,5 +1,7 @@
 <div class="bg-zinc-800 flex h-fit">
     <a class="border-white border-solid border-2 p-2 m-5 hover:bg-lime-900" href="/">Home</a>
-    <a class="border-white border-solid border-2 p-2 m-5 hover:bg-lime-900" href="/topList">Top songs</a>
+    <!-- <a class="border-white border-solid border-2 p-2 m-5 hover:bg-lime-900" href="/topList">Top songs</a> -->
     <a class="border-white border-solid border-2 p-2 m-5 hover:bg-lime-900" href="/stats">Stats</a>
+    <a class="border-white border-solid border-2 p-2 m-5 hover:bg-lime-900" href="/howToUse">How to use</a>
+
 </div>
---
+++ src/routes/howToUse/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+<h1>How to use</h1> 
+<div>
+    <section>
+       <h3>Amount shown</h3> 
+        <p>  
+            "start rank" is where from the data it will start looking from and the "number of songs" is how many songs after that should be shown.
+            The defualt vaules are "Start rank: 1" and "number of songs: 10" which will give you the top 10 songs.
+        </p> 
+    </section>
+    <section>
+        <h3>Filter</h3>
+        <p>
+            The filter you can choose beetween; All, Without Explicit and Explicit. This will filter the data to only show content with from choosen filter.
+        </p>
+    </section>
+    <section>
+        <h3>Artis filter</h3>
+        <p>
+            Either by clicking on the name of artist on the shown list or writing into the input field you can easily only filter from choosen arist. captialzine is not needed but correctly spell. You can as well add multipe artist with an "," beetween the name.
+        </p>
+    </section>
+</div>
+<style>
+    h1{
+        font-size: 3.5rem;
+    }
+    section{
+        margin-bottom: 1rem;
+        padding: 1rem;
+        background-color: rgba(100, 100, 100, 0.2);
+    }
+    h3{
+        font-size: 2rem;
+        font: bold;
+    }
+</style>
